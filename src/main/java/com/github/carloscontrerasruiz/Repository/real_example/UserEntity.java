6f0871a5d486efb28d93834d0d89b474f7113130
carloscontrerasruiz <carloscr9018@gmail.com>
2022-11-09 22:57:15 -0600

    Begin real example

--- src/main/java/com/github/carloscontrerasruiz/Repository/real_example/UserEntity.java
+++ src/main/java/com/github/carloscontrerasruiz/Repository/real_example/UserEntity.java
@@ -1,2 +1,9 @@
-package com.github.carloscontrerasruiz.Repository.real_example;public class UserEntity {
+package com.github.carloscontrerasruiz.Repository.real_example;
+
+import lombok.Data;
+
+@Data
+public class UserEntity {
+    private Integer id;
+    private String name;
 }
--- src/main/java/com/github/carloscontrerasruiz/Repository/real_example/UserService.java
+++ src/main/java/com/github/carloscontrerasruiz/Repository/real_example/UserService.java
@@ -1,2 +1,15 @@
-package com.github.carloscontrerasruiz.Repository.real_example;public interface UserService {
+package com.github.carloscontrerasruiz.Repository.real_example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public UserEntity saveUser(UserEntity user) {
+        return repository.save(user);
+    }
 }

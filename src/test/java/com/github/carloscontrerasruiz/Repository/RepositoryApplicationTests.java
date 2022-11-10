6fa230278ff17ea1ecc5948a933634cbc5eb1587
carloscontrerasruiz <carloscr9018@gmail.com>
2022-11-10 00:01:02 -0600

    Uso de un starter basico

--- src/test/java/com/github/carloscontrerasruiz/Repository/RepositoryApplicationTests.java
+++ src/test/java/com/github/carloscontrerasruiz/Repository/RepositoryApplicationTests.java
@@ -1,13 +1,20 @@
 package com.github.carloscontrerasruiz.Repository;
 
+import com.github.carloscontrerasruiz.user_starter.UserInfo;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RepositoryApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private UserInfo userInfo;
+
+    @Test
+    void starterTests() {
+        String userData = userInfo.getUserData("TheId");
+        System.out.println(userData);
+    }
 
 }
